7c8f384992aafaa6b77e6df28953f56d0cf3fc9a
wangkuixing <wangkx0902@163.com>
2020-11-30 15:21:14 +0800

    init

---
+++ src/com/binks/Main.java
@@ -0,0 +1,8 @@
+package com.binks;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ vars/execShell.groovy
@@ -0,0 +1,4 @@
+def call(params) {
+    // 让我们来执行个echo命令
+    sh "echo ${params}"
+}
